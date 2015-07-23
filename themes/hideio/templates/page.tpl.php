a7326f859f6524229f795d464369f89ab01a898e
TheodorosPloumis <me@theodorosploumis.com>
2015-07-23 19:16:32 +0300

    Theme tweaks

--- themes/hideio/templates/page.tpl.php
+++ themes/hideio/templates/page.tpl.php
@@ -74,66 +74,12 @@
 ?>
 <div class="page-wrapper">
 <div id="page">
-  <header id="header">
-    <div class="center-wrapper clearfix">
-      <?php if ($logo): ?>
-        <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" id="logo">
-          <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
-        </a>
-      <?php endif; ?>
-
-      <?php if ($site_name || $site_slogan): ?>
-        <div id="name-and-slogan">
-          <?php if ($site_name): ?>
-            <?php if ($title): ?>
-              <div id="site-name">
-                <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home">
-                  <?php print $site_name; ?>
-                </a>
-              </div>
-            <?php else: ?>
-              <h1 id="site-name">
-                <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home">
-                  <?php print $site_name; ?>
-                </a>
-              </h1>
-            <?php endif; ?>
-          <?php endif; ?>
-          <?php if ($site_slogan): ?>
-            <div id="site-slogan">
-              <?php print $site_slogan; ?>
-            </div>
-          <?php endif; ?>
-        </div>
-      <?php endif; ?>
-
-      <?php print render($page['header']); ?>
-    </div>
-  </header>
-
-  <?php if ($main_menu || $secondary_menu): ?>
-    <nav id="navigation">
-      <div class="center-wrapper clearfix">
-        <?php print theme('links__system_main_menu', array('links' => $main_menu, 'attributes' => array('id' => 'main-menu', 'class' => array('links', 'inline', 'clearfix')), 'heading' => t('Main menu'))); ?>
-        <?php print theme('links__system_secondary_menu', array('links' => $secondary_menu, 'attributes' => array('id' => 'secondary-menu', 'class' => array('links', 'inline', 'clearfix')), 'heading' => t('Secondary menu'))); ?>
-      </div>
-    </nav>
-  <?php endif; ?>
-
-  <?php if ($breadcrumb): ?>
-    <section id="breadcrumb">
-      <?php print $breadcrumb; ?>
-    </section>
-  <?php endif; ?>
 
   <?php print $messages; ?>
 
   <main id="main">
     <div class="center-wrapper clearfix">
       <section id="content">
-        <?php if ($page['highlighted']): ?>
-          <div id="highlighted"><?php print render($page['highlighted']); ?></div>
-        <?php endif; ?>
 
         <?php if ($tabs): ?>
           <div class="tabs"><?php print render($tabs); ?></div>
@@ -156,24 +102,8 @@
         <?php print $feed_icons; ?>
       </section>
 
-      <?php if ($page['sidebar_first']): ?>
-        <aside id="sidebar-first" class="column sidebar">
-          <?php print render($page['sidebar_first']); ?>
-        </aside>
-      <?php endif; ?>
-
-      <?php if ($page['sidebar_second']): ?>
-        <aside id="sidebar-second" class="column sidebar">
-          <?php print render($page['sidebar_second']); ?>
-        </aside>
-      <?php endif; ?>
     </div>
   </main>
 
-  <footer id="footer">
-    <div class="center-wrapper clearfix">
-      <?php print render($page['footer']); ?>
-    </div>
-  </footer>
 </div>
 </div>
